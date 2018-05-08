79788cf3f0308674d78c794bb426ee3a748cf165
Chaz Kondo <chuckeekondo@gmail.com>
2018-05-08 12:02:03 -1000

    Finished 1 but instead of click, used hover with mouseover and mouseout, and created a helper function

--- js/app.js
+++ js/app.js
@@ -4,9 +4,36 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+//add '.' before child class within function
+function makeHoverFunction(parent, child) {
 
+    var parentVar = document.getElementsByClassName(parent);
 
+for(var i = 0; i<parentVar.length; i++){
+  parentVar[i].addEventListener('mouseover', showMenu)
+}
 
+for(var i = 0; i<parentVar.length; i++){
+    parentVar[i].addEventListener('mouseout', hideMenu)
+  }
+
+function showMenu() {
+    var menuHover = this.querySelectorAll(child)[0];
+    //if(menuHover.style.display === 'block'){
+    //  menuHover.style.display = 'none';
+    //}else{
+      menuHover.style.display = 'block';
+    //}
+}
+
+function hideMenu() {
+    var menuHover = this.querySelectorAll(child)[0];
+      menuHover.style.display = 'none';
+}
+
+}
+
+makeHoverFunction('navi', '.inner')
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
