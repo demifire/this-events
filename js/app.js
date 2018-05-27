6f426a7c83cdb7c1a200e7a7252b40634fc7951e
Chaz Kondo <chuckeekondo@gmail.com>
2018-05-26 19:56:48 -1000

    Finished but did the third problem in a clunky way

--- js/app.js
+++ js/app.js
@@ -44,12 +44,56 @@ for(var i = 0; i<restElem.length; i++){
 }
 
 function showMenu(){
- var priceElem = this.querySelectorAll('.menu')[0];
-  if(priceElem.style.display === 'block'){
-    priceElem.style.display = 'none';
+ var menuElem = this.querySelectorAll('.menu')[0];
+  if(menuElem.style.display === 'block'){
+    menuElem.style.display = 'none';
   }else{
-    priceElem.style.display = 'block'
+    menuElem.style.display = 'block'
   }
 }
 
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/
+
+var thumb = document.getElementsByClassName('thumb')[0];
+var spanElem = document.createElement('span');
+spanElem.innerHTML = 0;
+spanElem.style.display = 'none';
+spanElem.style.whiteSpace = "nowrap";
+spanElem.style.color = 'red';
+thumb.style.display = 'inline';
+thumb.style.whiteSpace = "nowrap";
+thumb.appendChild(spanElem);
+thumb.addEventListener('click', function(){
+  console.log('test');
+  spanElem.style.display = 'inline-block';
+  spanElem.innerHTML ++;})
+
+var thumb1 = document.getElementsByClassName('thumb')[1];
+var spanElem1 = document.createElement('span');
+spanElem1.innerHTML = 0;
+spanElem1.style.display = 'none';
+spanElem1.style.whiteSpace = "nowrap";
+spanElem1.style.color = 'red';
+thumb1.style.display = 'inline';
+thumb1.style.whiteSpace = "nowrap";
+thumb1.appendChild(spanElem1);
+thumb1.addEventListener('click', function(){
+  console.log('test');
+  spanElem1.style.display = 'inline-block';
+  spanElem1.innerHTML ++;
+})
+
+var thumb2 = document.getElementsByClassName('thumb')[2];
+var spanElem2 = document.createElement('span');
+spanElem2.innerHTML = 0;
+spanElem2.style.display = 'none';
+spanElem2.style.whiteSpace = "nowrap";
+spanElem2.style.color = 'red';
+thumb2.style.display = 'inline';
+thumb2.style.whiteSpace = "nowrap";
+thumb2.appendChild(spanElem2);
+thumb2.addEventListener('click', function(){
+  console.log('test');
+  spanElem2.style.display = 'inline-block';
+  spanElem2.innerHTML ++;
+})
